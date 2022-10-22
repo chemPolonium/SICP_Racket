488c87d7bb2694399b5bc49dd08230995e2d3782
chemPolonium <chempolonium@outlook.com>
2022-10-22 22:50:33 +0800

    section 2.1.3

---
+++ section2.1.3.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(define (cons0 x y)
+  (lambda (m)
+    (cond ((= m 0) x)
+          ((= m 1) y)
+          (else (error "Argument not 0 or 1: CONS" m)))))
+(define (car0 z) (z 0))
+(define (cdr0 z) (z 1))
+
+(display "Exercise 2.4\n")
+(define (cons1 x y)
+  (lambda (m) (m x y)))
+(define (car1 z)
+  (z (lambda (p q) p)))
+(define (cdr1 z)
+  (z (lambda (p q) q)))
+
+(car1 (cons1 3 5))
+(cdr1 (cons1 3 5))
+
+(display "Exercise 2.5\n")
+(define (divisible-log p n)
+  (if (not (= (remainder n p) 0))
+      0
+      (+ 1 (divisible-log p (/ n p)))))
+
+(define (cons2 x y)
+  (* (expt 2 x)
+     (expt 3 y)))
+
+(define (car2 z)
+  (divisible-log 2 z))
+(define (cdr2 z)
+  (divisible-log 3 z))
+
+(car2 (cons2 3 5))
+(cdr2 (cons2 3 5))
+
+(display "Exercise 2.6")
+(define zero (lambda (f) (lambda (x) x)))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))

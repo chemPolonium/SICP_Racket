1eba667fdda6ae93288adf55a9c7fa824b050870
chemPolonium <chempolonium@outlook.com>
2023-06-04 19:38:40 +0800

    modify exercise 3.14

--- chapter3/e3-14.rkt
+++ chapter3/e3-14.rkt
@@ -4,7 +4,7 @@
   (define (loop x y)
     (if (null? x)
         y
-        (let ((temp (cdr x)))
+        (let ([temp (cdr x)])
           (set-cdr! x y)
           (loop temp x))))
   (loop x '()))

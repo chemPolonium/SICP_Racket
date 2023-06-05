70ad7acf34c6a06f305b887c8fc836b54f3ffbbb
chemPolonium <chempolonium@outlook.com>
2023-06-05 20:36:25 +0800

    add exercise 3.18

---
+++ chapter3/e3-18.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define (contains-cycle? x)
+  (define (cc-2 x l)
+    (if (not (pair? x))
+        #f
+        (or (memq x l)
+            (cc-2 (car x) (cons x l))
+            (cc-2 (cdr x) (cons x l)))))
+  (and (cc-2 x nil) #t))
+
+(define x (cons 'a 'b))
+
+(define return-3 (cons 'a (cons 'b (cons 'c nil))))
+
+(define return-4 (cons x (cons x 'c)))
+
+(define return-7 (cons (cons x x) (cons x x)))
+
+(define x1 (cons 'a 'b))
+(define x2 (cons 'a 'b))
+(define x3 (cons 'a 'b))
+(set-cdr! x1 x2)
+(set-cdr! x2 x3)
+(set-cdr! x3 x1)
+(define return-never x1)
+
+(contains-cycle? return-3)
+(contains-cycle? return-4)
+(contains-cycle? return-7)
+(contains-cycle? return-never)
